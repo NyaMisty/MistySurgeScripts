261ea82bb57ddee63ed6094aa8c15b51e221dd26
Misty <gyc990326@gmail.com>
2023-11-09 00:31:58 +0800

    Add OzanNoLogout

---
+++ OzanNoLogout/misty.ozannologout.js
@@ -0,0 +1,11 @@
+$done({
+    response: {
+	status: 200,
+	body: `{
+  "successful": true,
+  "referenceId": "00000000-0000-0000-0000-000000000000"
+}`,
+    headers: {
+        'content-type': 'application/json;charset=UTF-8'
+	},
+}
