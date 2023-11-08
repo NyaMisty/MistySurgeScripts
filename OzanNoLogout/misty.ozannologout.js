f9e1c45abe608d9521d1ce4b80737fe7f67592b9
Misty <gyc990326@gmail.com>
2023-11-09 01:00:03 +0800

    Fix typo in ozannologout

--- OzanNoLogout/misty.ozannologout.js
+++ OzanNoLogout/misty.ozannologout.js
@@ -1,11 +1,12 @@
 $done({
     response: {
-	status: 200,
-	body: `{
+	    status: 200,
+	    body: `{
   "successful": true,
   "referenceId": "00000000-0000-0000-0000-000000000000"
 }`,
-    headers: {
+        headers: {
         'content-type': 'application/json;charset=UTF-8'
-	},
-}
+        },
+    },
+})
